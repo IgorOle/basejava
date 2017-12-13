ad2b6f444ff82d77c79a1580fda9998c889c81ea
IgorOle <altconf@gmail.com>
2017-12-13 08:50:12 +0300

    correct get/delete

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private static int fullSize = 0;
+    private int fullSize = 0;
     Resume[] storage = new Resume[10000];
 
     private int findNumElement(String uuid) {
@@ -27,19 +27,12 @@ public class ArrayStorage {
 
 
     Resume get(String uuid) {
-        int pos;
-
-        pos = findNumElement(uuid);
-        if(pos >= 0) {
-            return storage[pos];
-        }
-        return null;
+        int pos = findNumElement(uuid);
+        return pos >= 0 ? storage[pos] : null;
     }
 
     void delete(String uuid) {
-        int pos;
-
-        pos = findNumElement(uuid);
+        int pos = findNumElement(uuid);
         if(pos >= 0) {
             // переносим крайний элемент на освободившее место (избавление от дырок)
             storage[pos] = storage[fullSize-1];
