76a35393431fe4df4e2173bc28dda81763b62fce
IgorOle <altconf@gmail.com>
2018-01-29 08:55:41 +0300

    add AbstractStorage, LimitStorage, LimitStorageTest

---
+++ src/com/igorole/basejava/webapp/storage/MapStorage.java
@@ -0,0 +1,46 @@
+package com.igorole.basejava.webapp.storage;
+
+import com.igorole.basejava.webapp.model.Resume;
+
+
+public class MapStorage extends AbstractStorage {
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return new Resume[0];
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    protected void doUpdate(int pos, Resume r) {
+
+    }
+
+    @Override
+    protected void insert(int pos, Resume r) {
+
+    }
+
+    @Override
+    protected void doDelete(int pos) {
+
+    }
+
+    @Override
+    protected int getIndex(String uuid) {
+        return 0;
+    }
+
+    @Override
+    protected Resume doGet(int pos) {
+        return null;
+    }
+}
