761f062001f0440cbb954e4a09f60f9dc5d5989d
IgorOle <altconf@gmail.com>
2018-02-23 20:47:18 +0300

    delete constructor from ContactType.java and SectionType.java

--- src/com/igorole/basejava/webapp/model/SectionType.java
+++ src/com/igorole/basejava/webapp/model/SectionType.java
@@ -1,16 +1,10 @@
 package com.igorole.basejava.webapp.model;
 
 public enum SectionType {
-    OBJECTIVE("Позиция"),
-    PERSONAL("Личные качества"),
-    ACHIEVEMENT("Достижения"),
-    QUALIFICATIONS("Квалификация"),
-    EXPERIENCE("Опыт работы"),
-    EDUCATION("Образование");
-
-    private String title;
-
-    SectionType(String title) {
-        this.title = title;
-    }
+    OBJECTIVE, //("Позиция"),
+    PERSONAL, //("Личные качества"),
+    ACHIEVEMENT, //("Достижения"),
+    QUALIFICATIONS, //("Квалификация"),
+    EXPERIENCE, //("Опыт работы"),
+    EDUCATION //("Образование");
 }
