c967e3d8f5e3afd85028785b60eaa1604431f689
IgorOle <altconf@gmail.com>
2018-03-13 11:46:34 +0300

    recursive with steps

--- src/com/igorole/basejava/webapp/MainFile.java
+++ src/com/igorole/basejava/webapp/MainFile.java
@@ -5,18 +5,18 @@ import java.io.File;
 public class MainFile {
     public static void main(String[] args) {
         File dir = new File(".");
-        treeDirectory(dir);
+        treeDirectory(dir, "+");
     }
 
-    public static void treeDirectory(File dir) {
+    public static void treeDirectory(File dir, String mark) {
         File[] files = dir.listFiles();
         if (files != null) {
             for (File file : files) {
                 if (file.isFile()) {
-                    System.out.println(file.getName());
+                    System.out.println(mark + file.getName());
                 } else if (file.isDirectory()) {
-                    System.out.println(file.getName());
-                    treeDirectory(file);
+                    System.out.println(mark + file.getName());
+                    treeDirectory(file, mark + "-");
                 }
             }
         }
