36a5b8df72b02212421f580402dcd1aa1b9850cf
IgorOle <altconf@gmail.com>
2018-04-24 11:51:55 +0300

    correct throws exception

--- src/com/igorole/basejava/webapp/sql/Executor.java
+++ src/com/igorole/basejava/webapp/sql/Executor.java
@@ -2,7 +2,8 @@ package com.igorole.basejava.webapp.sql;
 
 
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public interface Executor<T> {
-    T exec(PreparedStatement ps) throws Exception;
+    T exec(PreparedStatement ps) throws SQLException;
 }
--- src/com/igorole/basejava/webapp/sql/SqlHelper.java
+++ src/com/igorole/basejava/webapp/sql/SqlHelper.java
@@ -14,21 +14,17 @@ public class SqlHelper {
         this.connectionFactory = connectionFactory;
     }
 
-    public <T> T execute(String sql, Executor<T> executor){
+    public <T> T execute(String sql, Executor<T> executor) {
         try (Connection conn = connectionFactory.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             return executor.exec(ps);
         } catch (SQLException e) {
             if (e.getSQLState().equals("23505")) {
-                    throw new ExistStorageException(e.getMessage());
+                throw new ExistStorageException(e.getMessage());
             }
             throw new StorageException(e);
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        return null;
     }
-
 
 
 }
