1773523b4bd964524d1bd2c2fbfd2994f7bf45da
IgorOle <altconf@gmail.com>
2018-05-04 21:27:33 +0300

    lesson 14 implement SqlStorage.java with contacts

--- src/com/igorole/basejava/webapp/Test1.java
+++ src/com/igorole/basejava/webapp/Test1.java
@@ -1,56 +1,61 @@
-//package com.igorole.basejava.webapp;
-//
-//
-//import com.igorole.basejava.webapp.sql.SqlHelper;
-//
-//import java.sql.DriverManager;
-//import java.sql.PreparedStatement;
-//import java.util.concurrent.Callable;
-//import java.util.concurrent.CountDownLatch;
-//
-//class ThreadMy implements Callable {
-//    String name;
-//    CountDownLatch cd;
-//
-//    public ThreadMy(CountDownLatch cd, String name) {
-//        this.name = name;
-//        this.cd = cd;
-//    }
-//
-//    @Override
-//    public Object call() throws Exception {
-//        System.out.println("-");
-//        cd.countDown();
-//        cd.await();
-////        Thread.sleep(1000);
-//        System.out.println(System.currentTimeMillis());
-//        return name;
-//    }
-//}
-//
-//public class Test1 {
-//    static int inc = 0;
-//
-//    public static void main(String[] args) {
-//        SqlHelper sqlHelper = new SqlHelper(() -> DriverManager.getConnection(Config.get().getUrl(), Config.get().getUser(), Config.get().getPassword()));
-//        for (int i = 0; i < 10000; i++) {
-//
-//            sqlHelper.transactionalExecute(conn -> {
-//                try (PreparedStatement ps = conn.prepareStatement("insert into t1(id, dat) values(?,?)")) {
-//                    inc++;
-//                    ps.setInt(1, inc);
-//                    ps.setString(2, "t1_" + inc);
-//                    ps.execute();
-//
-//                }
-////                return null;
-////
-////                try (PreparedStatement ps = addContacts(conn, r)) {
-////                    ps.executeBatch();
-////                }
-////                return null;
-////            });
-//
+package com.igorole.basejava.webapp;
+
+
+import com.igorole.basejava.webapp.sql.SqlHelper;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+
+class ThreadMy implements Callable {
+    String name;
+    CountDownLatch cd;
+
+    public ThreadMy(CountDownLatch cd, String name) {
+        this.name = name;
+        this.cd = cd;
+    }
+
+    @Override
+    public Object call() throws Exception {
+        System.out.println("-");
+        cd.countDown();
+        cd.await();
+//        Thread.sleep(1000);
+        System.out.println(System.currentTimeMillis());
+        return name;
+    }
+}
+
+public class Test1 {
+    static int inc = 0;
+
+    public static void main(String[] args) {
+        SqlHelper sqlHelper = new SqlHelper(() -> DriverManager.getConnection(Config.get().getUrl(), Config.get().getUser(), Config.get().getPassword()));
+
+
+        sqlHelper.transactionalExecute(conn -> {
+
+            try (PreparedStatement ps = conn.prepareStatement("insert into t1(id, dat) values(?,?)")) {
+                for (int i = 0; i < 1000_000; i++) {
+                    ps.setInt(1, i);
+                    ps.setString(2, "t1_" + i);
+                    ps.execute();
+                }
+            }
+
+            try (PreparedStatement ps = conn.prepareStatement("insert into t2(id, dat222) values(?,?)")) {
+                for (int i = 0; i < 10_000; i++) {
+                    ps.setInt(1, i);
+                    ps.setString(2, "t2_" + i);
+                    ps.execute();
+                }
+            }
+            return null;
+
+        });
+
 //            inc = 0;
 //            for (int i = 0; i < 1000; i++) {
 //                sqlHelper.execute("insert into t2(id, dat) values(?,?)", ps -> {
@@ -61,7 +66,7 @@
 //                    return null;
 //                });
 //            }
-//
-//
-//        }
-//    }
+
+
+    }
+}
