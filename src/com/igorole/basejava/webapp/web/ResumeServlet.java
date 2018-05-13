9ec575c960ed3457e614851034775b14831dd1c0
Igo <altconf@gmail.com>
2018-05-13 20:25:33 +0300

    tmp

--- src/com/igorole/basejava/webapp/web/ResumeServlet.java
+++ src/com/igorole/basejava/webapp/web/ResumeServlet.java
@@ -1,11 +1,17 @@
 package com.igorole.basejava.webapp.web;
 
+import com.igorole.basejava.webapp.Config;
+import com.igorole.basejava.webapp.model.Resume;
+import com.igorole.basejava.webapp.storage.SqlStorage;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ResumeServlet extends HttpServlet {
+    SqlStorage storage;
+    Resume resume;
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
@@ -16,9 +22,9 @@ public class ResumeServlet extends HttpServlet {
         response.setContentType("text/html; charset=UTF-8");
         String uuid = request.getParameter("uuid");
         if(uuid != null) {
-
-//            response.getWriter().write();
+            storage = new SqlStorage(Config.get().getUrl(), Config.get().getUser(), Config.get().getPassword());
+            resume = storage.get(uuid);
+            response.getWriter().write(ToHTML.getResumeHTML(resume));
         }
-
     }
 }
--- src/com/igorole/basejava/webapp/web/ToHTML.java
+++ src/com/igorole/basejava/webapp/web/ToHTML.java
@@ -1,14 +1,23 @@
 package com.igorole.basejava.webapp.web;
 
+import com.igorole.basejava.webapp.model.ContactType;
+import com.igorole.basejava.webapp.model.Resume;
+
+import java.util.Map;
+
 public class ToHTML {
-    static public String getResumeHTML(String uuid) {
+    static public String getResumeHTML(Resume resume) {
         String result=""
                 ;
 
 
         return result;
     }
-    static private String getContacts(String uuid) {
+    static private String getContacts(Resume resume) {
+
+        for (Map.Entry<ContactType, String> entry: resume.getContacts(). entrySet()) {
+            System.out.println(entry.getValue());
+        }
         return "";
     }
 }
