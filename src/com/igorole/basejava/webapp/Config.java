07c529e0a9b4bcc4cc0dd88f645ec732d43f62b9
IgorOle <altconf@gmail.com>
2018-06-20 14:30:14 +0300

    first page - content Config.java read from resource

--- src/com/igorole/basejava/webapp/Config.java
+++ src/com/igorole/basejava/webapp/Config.java
@@ -1,13 +1,12 @@
 package com.igorole.basejava.webapp;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File("./config/resumes.properties");
+    private static final String PROPS = "/resumes.properties";
     private static final Config INSTANCE = new Config();
 
     private Properties props = new Properties();
@@ -22,14 +21,15 @@ public class Config {
     }
 
     private Config() {
-        try (InputStream is = new FileInputStream(PROPS)) {
+
+        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
             url = props.getProperty("db.url");
             user = props.getProperty("db.user");
             password = props.getProperty("db.password");
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file ");
         }
     }
 
